c98830dc6dbd6a30a39ba45a7caab8c8f9f3198a
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-10 17:37:36 -0300

    Add first interface and setup te aplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Task } from './components/interfaces';
 import './App.css';
 
+
+
 function App() {
+  const [task] = useState<Task[]>([
+    {
+      id:1,
+      title:'learn react',
+      description:'learn react',
+      completed:true
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {task.map(task =>  {
+       return <h1>{task.title}</h1>
+      })}
     </div>
   );
 }
---
+++ src/components/interfaces/index.ts
@@ -0,0 +1,7 @@
+
+export interface Task {
+    id: Number
+    title:String,
+    description:String
+    completed:Boolean
+  }
